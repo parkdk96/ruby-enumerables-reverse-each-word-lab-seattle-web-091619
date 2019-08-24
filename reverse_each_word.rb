94cc9afc0f3856fb7ba1faa665af0f534ff1604a
Daniel Park <parkdk96@gmail.com>
2019-08-24 17:49:13 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(string)
   string_arr = string.split(" ")
   reverse_string_array = []
   string_arr.collect do |n|
+    # each n is an element (string) from the string_arr
     reverse_string_array.push(n.reverse)
   end
   return reverse_string_array.join(" ")
